7f5a69897ce217a9c6b95f508a59c93d461ac0af
Vannestje <dennisvanneste@gmail.com>
2026-09-07 20:57:34 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,23 @@ android {
         applicationId = "com.someday.helios44"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.3.0"
+    }
+
+    signingConfigs {
+        create("stableDebug") {
+            storeFile = file("helios-debug.keystore")
+            storePassword = "helios44"
+            keyAlias = "helios44"
+            keyPassword = "helios44"
+        }
     }
 
     buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("stableDebug")
+        }
         release {
             isMinifyEnabled = false
         }
